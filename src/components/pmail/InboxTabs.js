fba56730d8215e73435e45363cdce9e9d301f6a4
ADev <amalchandrandev@gmail.com>
2018-08-28 04:01:03 +0530

    modified tabview used material tabs

--- src/components/pmail/InboxTabs.js
+++ src/components/pmail/InboxTabs.js
@@ -1,95 +1,113 @@
 import React, { Component } from "react";
-import {
-  Row,
-  Col,
-  Nav,
-  NavItem,
-  NavLink,
-  TabContent,
-  TabPane,
-  Card,
-  Button,
-  CardTitle,
-  CardText
-} from "reactstrap";
-import classnames from "classnames";
+import { withStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Icon from "@material-ui/core/Icon";
+import Typography from "@material-ui/core/Typography";
 
-export default class InboxTabs extends Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      activeTab: "1"
-    };
-  }
-
-  toggle(tab) {
-    if (this.state.activeTab !== tab) {
-      this.setState({
-        activeTab: tab
-      });
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper
+  },
+  tabsRoot: {
+    borderBottom: "1px solid #e8e8e8"
+  },
+  tabsIndicator: {
+    backgroundColor: "#1890ff"
+  },
+  tabRoot: {
+    textTransform: "initial",
+    minWidth: "13rem",
+    fontWeight: theme.typography.fontWeightRegular,
+    paddingRight: theme.spacing.unit * 2,
+    paddingLeft: theme.spacing.unit * 2,
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(","),
+    "&:hover": {
+      color: "#40a9ff",
+      opacity: 1
+    },
+    "&$tabSelected": {
+      color: "#1890ff",
+      fontWeight: theme.typography.fontWeightMedium
+    },
+    "&:focus": {
+      color: "#40a9ff"
     }
+  },
+  wrapper: {
+    flexDirection: "row",
+    justifyContent: "flex-start"
+  },
+  label: { fontWeight: 500 },
+  labelIcon: { minHeight: "48px" },
+  labelContainer: { paddingLeft: "4px" },
+
+  tabSelected: {},
+  typography: {
+    padding: theme.spacing.unit * 3
   }
+});
+
+class InboxTabs extends Component {
+  state = {
+    value: 0
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    const TabClasses = {
+      root: classes.tabRoot,
+      selected: classes.tabSelected,
+      wrapper: classes.wrapper,
+      label: classes.label,
+      labelContainer: classes.labelContainer,
+      labelIcon: classes.labelIcon
+    };
+
     return (
-      <div>
-        <Nav tabs>
-          <NavItem>
-            <NavLink
-              className={classnames({ active: this.state.activeTab === "1" })}
-              onClick={() => {
-                this.toggle("1");
-              }}
-            >
-              Tab1
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className={classnames({ active: this.state.activeTab === "2" })}
-              onClick={() => {
-                this.toggle("2");
-              }}
-            >
-              Moar Tabs
-            </NavLink>
-          </NavItem>
-        </Nav>
-        <TabContent activeTab={this.state.activeTab}>
-          <TabPane tabId="1">
-            <Row>
-              <Col sm="12">
-                <h4>Tab 1 Contents</h4>
-              </Col>
-            </Row>
-          </TabPane>
-          <TabPane tabId="2">
-            <Row>
-              <Col sm="6">
-                <Card body>
-                  <CardTitle>Special Title Treatment</CardTitle>
-                  <CardText>
-                    With supporting text below as a natural lead-in to
-                    additional content.
-                  </CardText>
-                  <Button>Go somewhere</Button>
-                </Card>
-              </Col>
-              <Col sm="6">
-                <Card body>
-                  <CardTitle>Special Title Treatment</CardTitle>
-                  <CardText>
-                    With supporting text below as a natural lead-in to
-                    additional content.
-                  </CardText>
-                  <Button>Go somewhere</Button>
-                </Card>
-              </Col>
-            </Row>
-          </TabPane>
-        </TabContent>
+      <div className={classes.root}>
+        <Tabs
+          value={value}
+          onChange={this.handleChange}
+          classes={{ root: classes.tabsRoot, indicator: classes.tabsIndicator }}
+        >
+          <Tab
+            // disableRipple
+            classes={TabClasses}
+            icon={<Icon>inbox</Icon>}
+            label="Primary"
+          />
+          <Tab
+            // disableRipple
+            icon={<Icon>people</Icon>}
+            classes={TabClasses}
+            label="Social"
+          />
+        </Tabs>
+        <Typography className={classes.typography}>
+          Ant Design UI powered by Material-UI
+        </Typography>
       </div>
     );
   }
 }
+
+export default withStyles(styles)(InboxTabs);
--- src/components/pmail/NavMailMain.js
+++ src/components/pmail/NavMailMain.js
@@ -1,25 +1,79 @@
 import React, { Component } from "react";
-import { Nav, NavItem, NavLink, Button } from "reactstrap";
+import { Icon } from "@material-ui/core";
+import MenuList from "@material-ui/core/MenuList";
+import MenuItem from "@material-ui/core/MenuItem";
+import { withStyles } from "@material-ui/core/styles";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Button from "@material-ui/core/Button";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import SendIcon from "@material-ui/icons/Send";
 
 export default class NavMailMain extends Component {
   render() {
     return (
       <div className="mt-3">
-        <Button className="mx-2" block>
+        <Button
+          className="mx-3 pr-4 text-capitalize bg-white"
+          variant="extendedFab"
+        >
+          <Icon>add</Icon>
           Compose
         </Button>
-        <Nav vertical>
-          <NavItem>
-            <NavLink href="#">Link</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Link</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Another Link</NavLink>
-          </NavItem>
-        </Nav>
+
+        <ListItemComposition />
       </div>
     );
   }
 }
+
+function ListItemComposition(props) {
+  const { classes } = props;
+
+  return (
+    <MenuList className="mt-3">
+      <MenuItem className={classes.menuItem}>
+        <ListItemIcon className={classes.icon}>
+          <InboxIcon />
+        </ListItemIcon>
+        <ListItemText classes={{ primary: classes.primary }} primary="Inbox" />
+      </MenuItem>
+      <MenuItem className={classes.menuItem}>
+        <ListItemIcon className={classes.icon}>
+          <SendIcon />
+        </ListItemIcon>
+        <ListItemText
+          classes={{ primary: classes.primary }}
+          primary="Sent mail"
+        />
+      </MenuItem>
+      <MenuItem className={classes.menuItem}>
+        <ListItemIcon className={classes.icon}>
+          <DraftsIcon />
+        </ListItemIcon>
+        <ListItemText classes={{ primary: classes.primary }} primary="Drafts" />
+      </MenuItem>
+    </MenuList>
+  );
+}
+
+const styles = theme => ({
+  menuItem: {
+    borderRadius: "50px",
+    borderBottomLeftRadius: "0px",
+    borderTopLeftRadius: "0px",
+    padding: "7px",
+    paddingLeft: "16px",
+    "&:focus": {
+      backgroundColor: "rgba(255,0,0,.1)",
+      "& $primary, & $icon": {
+        color: "#d93025"
+      }
+    }
+  },
+  primary: {},
+  icon: {}
+});
+
+ListItemComposition = withStyles(styles)(ListItemComposition);
