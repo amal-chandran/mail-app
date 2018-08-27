b8d048d4c80392586bc3822bc236494a35617fc4
ADev <amalchandrandev@gmail.com>
2018-08-28 04:16:50 +0530

    updated mail top menu

--- src/components/pmail/NavMailTop.js
+++ src/components/pmail/NavMailTop.js
@@ -1,32 +1,25 @@
 import React, { Component } from "react";
-import { Navbar, Nav, NavItem, NavLink } from "reactstrap";
+import { Row, Col } from "reactstrap";
+import { Checkbox, IconButton, Icon } from "@material-ui/core";
 
 export default class NavMailTop extends Component {
   render() {
     return (
-      <div>
-        <Navbar expand className="border-bottom">
-          <Nav navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
-              </NavLink>
-            </NavItem>
-          </Nav>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
-              </NavLink>
-            </NavItem>
-          </Nav>
-        </Navbar>
+      <div className="d-flex pt-2 border-bottom justify-content-between">
+        <div>
+          <Checkbox defaultChecked color="default" value="checkedG" />
+          <IconButton>
+            <Icon>refresh</Icon>
+          </IconButton>
+          <IconButton>
+            <Icon>more_vert</Icon>
+          </IconButton>
+        </div>
+        <div>
+          <IconButton>
+            <Icon>settings</Icon>
+          </IconButton>
+        </div>
       </div>
     );
   }
